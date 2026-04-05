90913c29c4cda1972b37e201b15aff0aaaf3ca62
Raegyeong Kim <kimrk3064@gmail.com>
2026-04-05 22:21:16 +0900

    bug-fix-4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def add(number_list):
     sum_value = 0
     for number in number_list:
         sum_value += number
-    returnsum_value
+    return sum_value
 
 def main():
     numbers = input("더할 숫자들을 공백으로 구분하여 입력하세요: ")
